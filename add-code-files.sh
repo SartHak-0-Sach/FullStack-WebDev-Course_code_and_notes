017771f27233a46338a1b8d7edc4bd3421c550b5
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-02-22 01:55:28 +0530

    Update add-code-files.sh

--- add-code-files.sh
+++ add-code-files.sh
@@ -6,11 +6,11 @@ parent_directory="C:/Users/saksh/OneDrive/Desktop/github/cwh-web-dev-playlist_co
 # Source directory to copy from
 source_directory="$parent_directory/lecture-001"
 
-# Loop through all folders except lecture-001
-for directory in "$parent_directory"/*/; do
-    if [ "$directory" != "$source_directory" ]; then
-        destination="${directory%?}"  # Removing trailing "/"
-        rsync -av "$source_directory/" "$destination"
+# Loop to copy contents to all other folders
+for folder in "$parent_directory"/*/; do
+    if [ "$folder" != "$source_directory" ]; then
+        destination="${folder%?}"  # Removing trailing "/"
+        cp -r "$source_directory"/* "$destination"
         echo "Copied contents of lecture-001 to $destination"
     fi
 done
